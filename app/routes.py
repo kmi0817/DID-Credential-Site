6c0b5cb27fd4292c38c5c684bfa9cadfb372054a
kmi0817 <kmi0817@naver.com>
2021-10-04 23:05:28 +0900

    signin page create

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template, redirect, url_for, session, request
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signin')
+def singin() :
+    return render_template('signin.html')
