cbcbf73cf5efb1a69e0dea1297c95ca9d99da214
kmi0817 <kmi0817@naver.com>
2021-11-11 10:36:29 +0900

    credential 발급 성공 (UI 정리X, 발급한 credential 확인페이지 아직)

--- app/routes.py
+++ app/routes.py
@@ -26,63 +26,64 @@ def tutorials() :
 
 @app.route('/credential')
 def credential() :
-    return render_template('credential.html')
+    conn = False
+    cred_def_ids = None
+    if 'connection' in session :
+        conn = True
+        with requests.get('http://0.0.0.0:8021/credential-definitions/created') as cred_def_res :
+            cred_def_ids = cred_def_res.json()['credential_definition_ids']
+            # print(cred_def_ids)
+    return render_template('credential.html', connection=conn, credential_definition_ids=cred_def_ids)
 
 @app.route('/credential-process', methods=['POST'])
 def credential_process() :
     cred_attrs = request.get_json(force=True) # 대표자가 전송한 증명서 관련 데이터
-    cred_def_id = cred_attrs['credential_definition_id']
-    cred_attrs['timestamp'] = str(int(time()))
-    del(cred_attrs['credential_definition_id'])
+    cred_def_id = cred_attrs['credential_definition_id'] # cred_def_id 가져오기
+    del(cred_attrs['credential_definition_id']) # cred_attrs에서 cred_def_id 제거
+    cred_attrs['timestamp'] = str(int(time())) # cred_attrs에 timestamp 추가
+    conn_id = session['connection']['conn_id'] # connectino id 가져오기
 
-    ## 발급기관(faber)와 플라스크 서버(alice) 간 연결 ##
-    with requests.post("http://0.0.0.0:8021/connections/create-invitation") as create_res :
-        print("faber: create-invitation OK")
-        invitation = create_res.json()['invitation'] # invitation 부분만 꺼내오기
-        conn_id = create_res.json()['connection_id'] # connection id만 꺼내오기
+    offer_request_body = {
+        "connection_id": conn_id,
+        "comment": f"Offer on cred def id {cred_def_id}",
+        "auto_remove": "false",
+        "credential_preview": {
+            "@type": "https://didcomm.org/issue-credential/2.0/credential-preview",
+            "attributes": [
+                {"name": n, "value": v}
+                for (n, v) in cred_attrs.items()
+            ]
+        },
+        "filter": {"indy": {"cred_def_id": cred_def_id}},
+        "trace": "false"
+    }
 
-        with requests.post("http://0.0.0.0:8031/connections/receive-invitation", json=invitation) as receive_res :
-            print("alice: receive-invitation OK")
+    with requests.post('http://0.0.0.0:8021/issue-credential-2.0/send-offer', json=offer_request_body) as offer_res :
+        result = offer_res.json()
+        cred_ex_id = result['cred_ex_id']
 
-            current = os.getcwd() # 현재 working directory 경로 가져오기
-            Woffer_request = os.path.join(current, 'app', 'static', 'offer_request.json')
-            file1 = open(Woffer_request, 'w')
-            offer_request_body = {
-                "connection_id": conn_id,
-                "comment": f"Offer on cred def id {cred_def_id}",
-                "auto_remove": "false",
-                "credential_preview": {
-                    "@type": "https://didcomm.org/issue-credential/2.0/credential-preview",
-                    "attributes": [
-                        {"name": n, "value": v}
-                        for (n, v) in cred_attrs.items()
-                    ]
-                },
-                "filter": {"indy": {"cred_def_id": cred_def_id}},
-                "trace": "false"
-            }
-            file1.write(str(offer_request_body))
-            file1.close()
+        working_directory = os.getcwd() # 현재 working directory 경로 가져오기
+        credential_file = os.path.join(working_directory, 'app', 'static', 'credential_file', cred_ex_id) # 경로 병합해 새 경로 생성
 
-            file2 = open(Woffer_request, 'r')
-            Roffer_request = file2.read()
-            file2.close()
+        with open(credential_file, 'w') as credential :
+            credential.write(str(result))
 
-            with requests.post('http://0.0.0.0:8021/issue-credential-2.0/send-offer', json=Roffer_request) as offer_res :
-                print("faber: issue-credential OK")
-                result = json.loads(offer_res.text)
-                print(result)
-            #     result = json.loads(offer_res.text)
+    return cred_ex_id
 
-    return result
+@app.route('/credential/<cred_ex_id>')
+def credential_cred_ex_id(cred_ex_id) :
+    working_directory = os.getcwd() # 현재 working directory 경로 가져오기
+    credential_file = os.path.join(working_directory, 'app', 'static', 'credential_file', cred_ex_id) # 경로 병합해 새 경로 생성
 
-@app.route('/credential-issued')
-def credential_issued() :
-    return render_template('credential_issued.html')
+    with open(credential_file, 'r') as credential :
+        credential_body = credential.read()
+    return render_template('credential_issued.html', cred_ex_id=cred_ex_id, credential_body=credential_body)
 
 @app.route('/chat')
 def chatting() :
     return render_template('chat.html')
+
+
 
 
 ## 발급기관(faber)와 플라스크 서버(alice) 간 연결 ##
@@ -100,6 +101,20 @@ def create_connection() :
         "conn_id": conn_id,
         "my_did": my_did
     }
+    session['connection'] = ret
+    return ret
+
+@app.route('/session-pop', methods=['POST'])
+def session_pop() :
+    session.clear()
+    return 'session pop'
+
+@app.route('/delete-connection', methods=['DELETE'])
+def delete_connection() :
+    conn_id = session['connection']['conn_id']
+    with requests.delete(f'http://0.0.0.0:8021/connections/{conn_id}') as delete_res :
+        ret = delete_res.json()
+        session.pop('connection', None)
     return ret
 
 @app.route('/create-schema', methods=['POST'])
